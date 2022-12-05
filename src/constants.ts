6bccc555bbecdaae5d084a28fbe30f33748c9620
ashiqsultan <ashiqdrive@gmail.com>
2022-12-05 18:50:32 +0530

    updated api url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-export const API_URL = 'http://localhost:5000';
+// export const API_URL = 'http://localhost:5000';
+export const API_URL = 'https://delivery-service-backend.onrender.com';
+
 export const socketEvents = {
   SHIPMENT_CREATED: 'SHIPMENT_CREATED',
   UPDATE_DA_LOCATION: 'UPDATE_DA_LOCATION',
